7c02827277bbf478eafff8a7a7dfd998b5a31f4e
mysticmind <babu.annamalai@gmail.com>
2017-05-23 13:40:50 +0530

    Enable tests in appveyor config

--- build.sh
+++ build.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 set -ev
 
-#npm install
-dotnet --info
-
 dotnet restore ./src/Marten.sln --runtime netstandard1.3
 dotnet build ./src/Marten.Testing/Marten.Testing.csproj --framework netcoreapp1.0 --configuration Release
-#npm run test
-#dotnet test ./src/Marten.Testing/Marten.Testing.csproj --framework netcoreapp1.0 --configuration Release 
+npm run test
+dotnet test ./src/Marten.Testing/Marten.Testing.csproj --framework netcoreapp1.0 --configuration Release 
